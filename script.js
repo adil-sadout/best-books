c5254a69deb468e5d1cfe51a173db1f5ee619b5c
Adil Sadout <adelsadout@hotmail.com>
2020-05-23 18:29:24 +0100

    removed the service worker

--- script.js
+++ script.js
@@ -101,16 +101,3 @@ function scrollFunction() {
 
 
 
-
-  
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
