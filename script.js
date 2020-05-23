b4eb421886dda8342b641c28a3c59c7052f3b16c
Adil Sadout <adelsadout@hotmail.com>
2020-05-23 04:20:24 +0100

    added buttons & small modal that shows its a prototype

--- script.js
+++ script.js
@@ -5,17 +5,13 @@
 
 
 f = document.getElementsByClassName('analy')[0];
-console.log(f);
-    
-
 document.querySelectorAll('.nav-buttons button').forEach(item => {
     
     item.addEventListener('click', event => {
         let k = document.getElementsByClassName(item.id)[0];
         f.classList.add("hidden");
         k.classList.remove("hidden");
-        console.log(item);
-        f= k;
+        f=k;
         
     });
     
@@ -23,18 +19,90 @@ document.querySelectorAll('.nav-buttons button').forEach(item => {
 
 
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
+
+
+
+modal = document.getElementsByClassName("modal")[0];
+modalbtn = document.getElementsByClassName("modal-button")[0];
+
+console.log(modalbtn);
+
+document.querySelectorAll(".buy-buttons button").forEach(button=> {
+    button.addEventListener("click",() =>{
+      modal.classList.remove("hidden");
     });
-  }
+});
+
+modalbtn.addEventListener("click",() =>{
+  modal.classList.add("hidden");
+});
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function backToTop(){
+    document.body.scrollTop = 0;  // For Safari
+    document.documentElement.scrollTop = 0;  // For Chrome, Firefox, IE and Opera
+}
+function scrollFunction() {
+    btn=document.getElementsByClassName("button-up")[0];
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+}
+  window.onscroll = function() {scrollFunction()};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ var cacheName = 'v1';
 // Default files to always cache
 var cacheFiles = [
 	'./',
-	'./index.html',
 	'./script.js',
 	'./style.css',
 ]
